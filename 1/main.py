07acb6dae4082df8197be34289b0b454dba0aaad
Sheldon Nunes <Sheldon.Nunes@fphcare.co.nz>
2018-12-03 13:47:04 +1300

    Change adds solution to 1b)

--- 1/main.py
+++ 1/main.py
@@ -6,3 +6,16 @@ for value in values:
     total = total + int(value)
 
 print(total)
+
+current_frequencey = 0
+observed_frequencies = set()
+
+lookingForDouble = True
+while lookingForDouble:
+    for value in values:
+        if current_frequencey in observed_frequencies:
+            print(current_frequencey)
+            lookingForDouble = False
+            break
+        observed_frequencies.add(current_frequencey)
+        current_frequencey += int(value)
