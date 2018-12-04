78950beaa628d9e77ab439f213fd3f9309fb7d5c
Sheldon Nunes <Sheldon.Nunes@fphcare.co.nz>
2018-12-05 09:05:59 +1300

    change adds solution to 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -1,3 +1,5 @@
+# Exercise 1
+
 def containsDuplicatesOfSameLetter(text, numberOfDuplicates):
     sorted_text = ''.join(sorted(text))
     current_letter = sorted_text[0]
@@ -13,6 +15,7 @@ def containsDuplicatesOfSameLetter(text, numberOfDuplicates):
     if count == numberOfDuplicates:
         return True
     return False
+
 
 with open('input.txt', 'r') as f:
     codes = f.readlines()
@@ -33,24 +36,26 @@ print(two_of_letter_count * three_of_letter_count)
 
 
 
-checksums = []
-for code in codes:
-    checksum = 0
-    for index, letter in enumerate(code):
-        checksum += ord(letter) * (index + 1) * (index + 1)
-    checksums.append(checksum)
 
-print(checksums)
-
-checksums.sort()
-
-for i in range(1, len(checksums)):
-    if(checksums[i] - checksums[i-1] < 1):
-        print(checksums[i])
-        print('Hello')
+# Exercise 2
 
 
-#go through list and create a checksum
-#sort in order
-#find numbers that are close by
+def containsOnlyOneDifference(box1, box2):
+    count = 0
+    for i in range(0, len(box1)):
+        if(box1[i] != box2[i]):
+            count += 1
+            if count > 1:
+                return False
+    return True
+        
+
+
+
+for i in range(0, len(codes)):
+    for j in range(i+1, len(codes)):
+        result = containsOnlyOneDifference(codes[i], codes[j])
+        if(result):
+            print('Code 1:  ' + codes[i])
+            print('Code 2:  ' + codes[j])
 
