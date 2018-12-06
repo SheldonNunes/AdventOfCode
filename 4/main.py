3245a83f5ac4b76e150642f2f5e5a1d3cf3ae18d
Sheldon Nunes <Sheldon.Nunes@fphcare.co.nz>
2018-12-07 09:01:30 +1300

    change adds solution to part 4

--- 4/main.py
+++ 4/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import operator
 
 with open('input.txt', 'r') as f:
     lines = f.readlines()
@@ -54,12 +55,38 @@ for entry in entries:
     if(isWakeEvent(entry.event)):
         delta = entry.timestamp - sleepTimeStamp
         if(currentGuard in guardSleep):
-            print('MINUTES: ' + str((delta.seconds//60)%60))
+            print('GUARD: ' + str(currentGuard) + ', MINUTES: ' + str((delta.seconds//60)%60))
             guardSleep[currentGuard] += (delta.seconds//60)%60
         else:
             guardSleep[currentGuard] = (delta.seconds//60)%60
 print(guardSleep)
+max_id = max(guardSleep, key=guardSleep.get)
+print('Max: ' + max_id)
 
-print('Max: ' + max(guardSleep, key=guardSleep.get))
 
-print(650*401)
+minutes = []
+for i in range(60):
+    minutes.append(0)
+
+for entry in entries:
+    if(isGuardEvent(entry.event)):
+        currentGuard = getGuardId(entry.event)
+    if(isSleepEvent(entry.event)):
+        sleepTimeStamp = entry.timestamp
+    if(isWakeEvent(entry.event)):
+        if(currentGuard == max_id):
+            for i in range(sleepTimeStamp.minute, entry.timestamp.minute):
+                minutes[i] += 1
+
+print(len(minutes))
+print(minutes)
+print(guardSleep[max_id])
+
+index, value = max(enumerate(minutes), key=operator.itemgetter(1))
+
+print(index)
+print(max_id)
+print(index * int(max_id))
+
+
+# print(max_id * guardSleep[max_id])
