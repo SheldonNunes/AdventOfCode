6501d2bdc0c2824af280d25a6ee65c8ccc4d5d00
Sheldon Nunes <Sheldon.Nunes@fphcare.co.nz>
2018-12-10 16:12:20 +1300

    Added solution to day 5 part 2

--- 5/main.py
+++ 5/main.py
@@ -1,5 +1,5 @@
-with open('input.txt', 'r') as f:
-    data = f.readline()
+# with open('input.txt', 'r') as f:
+#     data = f.readline()
 
 def findReaction(data):
     for i in range(1, len(data)):
@@ -10,16 +10,42 @@ def findReaction(data):
     return None
 
 
-while(True):
-    reaction = findReaction(data)
-    if(reaction is None):
-        break
+# while(True):
+#     reaction = findReaction(data)
+#     if(reaction is None):
+#         break
     
-    left = reaction[0]
-    right = reaction[1]
+#     left = reaction[0]
+#     right = reaction[1]
 
-    data = data[0:left] + data[right+1:]
+#     data = data[0:left] + data[right+1:]
 
-print('HELLO')
-print(data)
-print(len(data))
+# print('HELLO')
+# print(data)
+# print(len(data))
+
+# PART 2
+
+with open('part2.txt', 'r') as f:
+    data = f.readline()
+
+shortestLength = len(data)
+for i in range(ord('A'), ord('z')):
+    char_to_remove = chr(i)
+    filtered_data = data.replace(char_to_remove.lower(), "")
+    filtered_data = filtered_data.replace(char_to_remove.upper(), "")
+
+    while(True):
+        reaction = findReaction(filtered_data)
+        if(reaction is None):
+            break
+        
+        left = reaction[0]
+        right = reaction[1]
+
+        filtered_data = filtered_data[0:left] + filtered_data[right+1:]
+
+    if(len(filtered_data) < shortestLength):
+        shortestLength = len(filtered_data)
+
+print(shortestLength)
